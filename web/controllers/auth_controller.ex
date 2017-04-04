823f7d79561f733a9d131b26a3f6a4d74da59842
Sam Houston <shouston3@me.com>
2017-04-04 17:46:43 +0100

    fixes issue #18

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,4 +1,5 @@
 defmodule Auth.AuthController do
+  use Auth.Web, :controller
   import Plug.Conn
   import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
   plug Ueberauth
@@ -53,7 +54,7 @@ defmodule Auth.AuthController do
 
   def login(conn, user) do
     conn
-    |> put_current_user(user)
+    |> assign(:current_user, user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
