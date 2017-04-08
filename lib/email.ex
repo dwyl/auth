3ec96abc93bccfce8d312dd6b8648789ba02edc0
nelsonic <contact.nelsonic+github@gmail.com>
2017-04-08 03:14:36 +0100

    adds validate_email.ex and corresponding test

--- lib/email.ex
+++ lib/email.ex
@@ -1,10 +1,10 @@
 defmodule Auth.Email do
   use Bamboo.Phoenix, view: Auth.EmailView
 
-  def send_test_email(from_email_address, subject, message) do
+  def send_test_email(to_email_address, subject, message) do
     new_email()
-    |> to("nelson@dwyl.io")
-    |> from(from_email_address) # also needs to be a validated email
+    |> from("nelson@dwyl.io") # also needs to be a validated email
+    |> to(to_email_address)
     |> subject(subject)
     |> text_body(message)
   end
