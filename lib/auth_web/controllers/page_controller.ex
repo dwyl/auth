c24889c7e64b0970d1fde032b6d892dd315eec4f
nelsonic <nelson+github@dwyl.io>
2020-04-03 22:09:53 +0100

    create get_referer/1 function to extract HTTP referer

--- lib/auth_web/controllers/page_controller.ex
+++ lib/auth_web/controllers/page_controller.ex
@@ -1,15 +1,28 @@
 defmodule AuthWeb.PageController do
   use AuthWeb, :controller
 
-  @elixir_auth_google Application.get_env(:auth_mvp, :elixir_auth_google) || ElixirAuthGoogle
-  @elixir_auth_github Application.get_env(:auth_mvp, :elixir_auth_github) || ElixirAuthGithub
-
   def index(conn, _params) do
-    oauth_github_url = ElixirAuthGithub.login_url(%{scopes: ["user:email"]})
+    get_referer(conn)
+    oauth_github_url =
+      ElixirAuthGithub.login_url(%{scopes: ["user:email"]})
     oauth_google_url = ElixirAuthGoogle.generate_oauth_url(conn)
     render(conn, "index.html", [
       oauth_github_url: oauth_github_url,
       oauth_google_url: oauth_google_url
     ])
   end
+
+
+  def get_referer(conn) do
+    # IO.inspect(conn, label: "extact_referer/1:16 conn")
+    # https://stackoverflow.com/questions/37176911/get-http-referrer
+    case List.keyfind(conn.req_headers, "referer", 0) do
+      {"referer", referer} ->
+        IO.puts referer
+      nil ->
+        IO.puts "no referer"
+    end
+    conn
+  end
+
 end
