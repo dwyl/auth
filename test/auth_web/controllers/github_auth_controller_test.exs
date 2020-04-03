556f80e0155528334a46ac3d6f0c8c46670bba00
nelsonic <nelson+github@dwyl.io>
2020-04-03 22:09:10 +0100

    use latest version of auth packages 1.2.0 with TestDouble simplifies code and tests!

--- test/auth_web/controllers/github_auth_controller_test.exs
+++ test/auth_web/controllers/github_auth_controller_test.exs
@@ -5,12 +5,6 @@ defmodule AuthWeb.GithubAuthControllerTest do
 
     conn = get(conn, Routes.github_auth_path(conn, :index,
       %{code: "123", state: "http://localhost/"}))
-    # IO.inspect(conn, label: "conn")
-
     assert html_response(conn, 200) =~ "test@gmail.com"
-
-    # # same again to exercise to the branch where person already exists:
-    # conn = get(conn, Routes.google_auth_path(conn, :index, %{code: "234", state: "http://localhost/"}))
-    # assert html_response(conn, 302)
   end
 end
