1beb0e37487853eb5facd52ea503d88580d7d8b3
nelsonic <nelson+github@dwyl.io>
2020-04-13 23:17:44 +0100

    add start of seed to insert Admin person https://github.com/dwyl/auth/issues/47

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,10 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Auth.Person
+alias Auth.Repo
+IO.inspect(System.get_env("ADMIN_EMAIL"), label: "ADMIN_EMAIL")
+
+%Person{email: System.get_env("ADMIN_EMAIL")}
+|> Person.changeset(%{email: System.get_env("ADMIN_EMAIL")})
+|> Repo.insert!()
