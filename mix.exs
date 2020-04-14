b62faa1d5d8cacdcfab7731141d9ac790d1d8fe6
nelsonic <nelson+github@dwyl.io>
2020-04-14 21:31:11 +0100

    run seeds script in tests see: https://github.com/dwyl/auth/issues/47#issuecomment-613665596

--- mix.exs
+++ mix.exs
@@ -82,7 +82,7 @@ defmodule Auth.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "run priv/repo/seeds.exs", "test"]
     ]
   end
 
