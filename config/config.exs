04f13ed07004aae4fbd0a46d1247424bd80bb6ea
nelsonic <nelson+github@dwyl.io>
2020-04-15 22:14:42 +0100

    add config :joken for signing tokens #12

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,9 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# https://hexdocs.pm/joken/introduction.html#usage
+config :joken, default_signer: System.get_env("SECRET_KEY_BASE")
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
