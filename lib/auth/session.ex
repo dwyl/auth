e89e5608dc803d96c2fd3d8e58ea8d16d0a5d1e2
nelsonic <nelson+github@dwyl.io>
2020-04-16 21:07:23 +0100

    temporarily comment out tag and session as not (yet) used

--- lib/auth/session.ex
+++ lib/auth/session.ex
@@ -1,23 +1,23 @@
-defmodule Auth.Session do
-  use Ecto.Schema
-  import Ecto.Changeset
-
-  schema "sessions" do
-    field :auth_token, Fields.Encrypted
-    field :refresh_token, Fields.Encrypted
-    field :key_id, :integer
-
-    belongs_to :person, Auth.Person
-    timestamps()
-  end
-
-  def changeset(people, attrs) do
-    Ecto.build_assoc(people, :sessions)
-    |> cast(attrs, [:auth_token, :refresh_token])
-    |> validate_required([:auth_token, :refresh_token])
-  end
-
-  def basic_changeset(people, _attrs) do
-    Ecto.build_assoc(people, :sessions)
-  end
-end
+# defmodule Auth.Session do
+#   use Ecto.Schema
+#   import Ecto.Changeset
+#
+#   schema "sessions" do
+#     field :auth_token, Fields.Encrypted
+#     field :refresh_token, Fields.Encrypted
+#     field :key_id, :integer
+#
+#     belongs_to :person, Auth.Person
+#     timestamps()
+#   end
+#
+#   def changeset(people, attrs) do
+#     Ecto.build_assoc(people, :sessions)
+#     |> cast(attrs, [:auth_token, :refresh_token])
+#     |> validate_required([:auth_token, :refresh_token])
+#   end
+#
+#   def basic_changeset(people, _attrs) do
+#     Ecto.build_assoc(people, :sessions)
+#   end
+# end
--- lib/auth/tag.ex
+++ lib/auth/tag.ex
@@ -1,17 +1,17 @@
-defmodule Auth.Tag do
-  use Ecto.Schema
-  import Ecto.Changeset
-
-  schema "tags" do
-    field :text, :string
-    belongs_to :person, Auth.Person
-    timestamps()
-  end
-
-  @doc false
-  def changeset(tag, attrs) do
-    tag
-    |> cast(attrs, [:text])
-    |> validate_required([:text])
-  end
-end
+# defmodule Auth.Tag do
+#   use Ecto.Schema
+#   import Ecto.Changeset
+#
+#   schema "tags" do
+#     field :text, :string
+#     belongs_to :person, Auth.Person
+#     timestamps()
+#   end
+#
+#   @doc false
+#   def changeset(tag, attrs) do
+#     tag
+#     |> cast(attrs, [:text])
+#     |> validate_required([:text])
+#   end
+# end
