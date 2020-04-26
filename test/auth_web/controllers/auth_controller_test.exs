78ac64261f89a8236576cb6d71f1dcbd522e51f2
nelsonic <nelson+github@dwyl.io>
2020-04-26 20:25:30 +0100

    auth_controller back up to 100%

--- test/auth_web/controllers/auth_controller_test.exs
+++ test/auth_web/controllers/auth_controller_test.exs
@@ -1,5 +1,6 @@
 defmodule AuthWeb.AuthControllerTest do
   use AuthWeb.ConnCase
+  # @email System.get_env("ADMIN_EMAIL")
 
   test "github_handler/2 github auth callback", %{conn: conn} do
     conn = get(conn, "/auth/github/callback",
@@ -23,4 +24,33 @@ defmodule AuthWeb.AuthControllerTest do
     # assert html_response(conn, 200) =~ "nelson@gmail.com"
     assert html_response(conn, 302) =~ "redirected"
   end
+
+  test "google_handler/2 show welcome (state=nil) > handler/3", %{conn: conn} do
+    data = %{
+      email: "nelson@gmail.com",
+      givenName: "McTestin",
+      picture: "https://youtu.be/naoknj1ebqI",
+      auth_provider: "google"
+    }
+    person = Auth.Person.create_person(data) # |> IO.inspect(label: "person")
+    conn = AuthPlug.create_jwt_session(conn, Map.merge(data, %{id: person.id}))
+    conn = get(conn, "/auth/google/callback",
+      %{code: "234", state: nil})
+
+    assert html_response(conn, 200) =~ "google account"
+    # assert html_response(conn, 302) =~ "redirected"
+  end
+
+  # test "handler/3 show welcome page", %{conn: conn} do
+  #
+  #   person = Auth.Person.create_person(%{
+  #     email: "test@gmail.com", givenName: "McTestin"
+  #   })
+  #   IO.inspect(person, label: "person")
+  #
+  #   conn = AuthPlug.create_jwt_session(conn, %{email: person.email, id: person.id})
+  #   conn = AuthWeb.AuthController.handler(conn, person, nil)
+  #   IO.inspect(conn, label: "conn")
+  #   # assert html_response(conn, 302) =~ "redirected"
+  # end
 end
