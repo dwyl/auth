36f87d48d322b5a4de605eadcf2a5ccf450d4ad3
nelsonic <nelson+github@dwyl.io>
2020-04-26 13:24:14 +0100

    render DWYL_API_KEY from client_id and client_secret #42

--- lib/auth_web/controllers/apikey_controller.ex
+++ lib/auth_web/controllers/apikey_controller.ex
@@ -33,14 +33,10 @@ defmodule AuthWeb.ApikeyController do
   def create(conn, %{"apikey" => apikey_params}) do
     IO.inspect(apikey_params, label: "apikey_params")
     person_id = conn.assigns.decoded.id
-    secret = Fields.AES.encrypt(person_id) |> Base58.encode
-    |> IO.inspect(label: "secret")
 
-    client = Fields.AES.encrypt(person_id) |> Base58.encode
-    |> IO.inspect(label: "client")
     params = Map.merge(apikey_params, %{
-      "client_secret" => secret,
-      "client_id" => client,
+      "client_secret" => encrypt_encode(person_id),
+      "client_id" => encrypt_encode(person_id),
       "person_id" => person_id
       })
     case Apikey.create_apikey(params) do
