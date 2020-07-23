e961a173365837edd15c0ce4b15c8f4d59a07b6d
nelsonic <nelson+github@dwyl.com>
2020-07-23 11:57:38 +0100

    add AuthTest.admin_login/1 helper function to login as admin https://github.com/dwyl/auth/issues/27#issuecomment-662895801

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,17 @@
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Auth.Repo, :manual)
+
+
+defmodule AuthTest do
+  @moduledoc """
+  Test helper functions :-)
+  """
+  @admin_email System.get_env("ADMIN_EMAIL")
+  @doc """
+  add a valid JWT/session to the conn for routes that require auth as "SuperAdmin"
+  """
+  def admin_login(conn) do
+    person = Auth.Person.get_person_by_email(@admin_email)
+    AuthPlug.create_jwt_session(conn, person)
+  end
+end
