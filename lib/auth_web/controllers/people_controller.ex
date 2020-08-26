2eacfeb2b95d11d84247cba0738cc753ac811b06
nelsonic <nelson+github@dwyl.com>
2020-08-26 08:33:47 +0100

    tidy up PeopleController.show/2 handler https://github.com/dwyl/auth/pull/85#pullrequestreview-475217260

--- lib/auth_web/controllers/people_controller.ex
+++ lib/auth_web/controllers/people_controller.ex
@@ -29,15 +29,11 @@ defmodule AuthWeb.PeopleController do
     # should be visible to superadmin and people with "admin" role
     if conn.assigns.person.id == 1 do
       person = Auth.Person.get_person_by_id(Map.get(params, "person_id"))
-      roles = Auth.PeopleRoles.get_roles_for_person(person.id)
-      IO.inspect(roles, label: "roles")
-      all_roles = Auth.Role.list_roles()
-
       render(conn, :profile,
         person: person,
-        roles: roles,
+        roles: Auth.PeopleRoles.get_roles_for_person(person.id),
         statuses: Auth.Status.list_statuses(),
-        all_roles: all_roles
+        all_roles: Auth.Role.list_roles()
       )
 
       # Note: this can easily be refactored to save on DB queries. #HelpWanted
