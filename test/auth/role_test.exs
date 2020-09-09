8e6c096043fb5467d53d23034e031710f9ebb1a2
nelsonic <nelson+github@dwyl.com>
2020-09-09 08:41:44 +0100

    remove single pipeline operator from permissions tests to keep @sourcelevel-bot happy https://github.com/dwyl/technology-stack/issues/86

--- test/auth/role_test.exs
+++ test/auth/role_test.exs
@@ -20,7 +20,7 @@ defmodule Auth.RoleTest do
 
     test "list_roles/0 returns all roles" do
       role = role_fixture()
-      assert Role.list_roles() |> List.last() == role
+      assert List.last(Role.list_roles()) == role
     end
 
     test "get_role!/1 returns the role with given id" do
--- test/auth_web/controllers/permission_controller_test.exs
+++ test/auth_web/controllers/permission_controller_test.exs
@@ -14,23 +14,24 @@ defmodule AuthWeb.PermissionControllerTest do
 
   describe "index" do
     test "lists all permissions", %{conn: conn} do
-      conn = admin_login(conn) |> get(Routes.permission_path(conn, :index))
+      conn = admin_login(conn)
+      conn = get(conn, Routes.permission_path(conn, :index))
       assert html_response(conn, 200) =~ "Listing Permissions"
     end
   end
 
   describe "new permission" do
     test "renders form", %{conn: conn} do
-      conn = admin_login(conn) |> get(Routes.permission_path(conn, :new))
+      conn = admin_login(conn)
+      conn = get(conn, Routes.permission_path(conn, :new))
       assert html_response(conn, 200) =~ "New Permission"
     end
   end
 
   describe "create permission" do
     test "redirects to show when data is valid", %{conn: conn} do
-      conn =
-        admin_login(conn)
-        |> post(Routes.permission_path(conn, :create), permission: @create_attrs)
+      conn = admin_login(conn)
+      conn = post(conn, Routes.permission_path(conn, :create), permission: @create_attrs)
 
       assert %{id: id} = redirected_params(conn)
       assert redirected_to(conn) == Routes.permission_path(conn, :show, id)
@@ -40,9 +41,8 @@ defmodule AuthWeb.PermissionControllerTest do
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      conn =
-        admin_login(conn)
-        |> post(Routes.permission_path(conn, :create), permission: @invalid_attrs)
+      conn = admin_login(conn)
+      conn = post(conn, Routes.permission_path(conn, :create), permission: @invalid_attrs)
 
       assert html_response(conn, 200) =~ "New Permission"
     end
@@ -52,9 +52,8 @@ defmodule AuthWeb.PermissionControllerTest do
     setup [:create_permission]
 
     test "renders form for editing chosen permission", %{conn: conn, permission: permission} do
-      conn =
-        admin_login(conn)
-        |> get(Routes.permission_path(conn, :edit, permission))
+      conn = admin_login(conn)
+      conn = get(conn, Routes.permission_path(conn, :edit, permission))
 
       assert html_response(conn, 200) =~ "Edit Permission"
     end
@@ -64,9 +63,8 @@ defmodule AuthWeb.PermissionControllerTest do
     setup [:create_permission]
 
     test "redirects when data is valid", %{conn: conn, permission: permission} do
-      conn =
-        admin_login(conn)
-        |> put(Routes.permission_path(conn, :update, permission), permission: @update_attrs)
+      conn = admin_login(conn)
+      conn = put(conn, Routes.permission_path(conn, :update, permission), permission: @update_attrs)
 
       assert redirected_to(conn) == Routes.permission_path(conn, :show, permission)
 
@@ -75,9 +73,8 @@ defmodule AuthWeb.PermissionControllerTest do
     end
 
     test "renders errors when data is invalid", %{conn: conn, permission: permission} do
-      conn =
-        admin_login(conn)
-        |> put(Routes.permission_path(conn, :update, permission), permission: @invalid_attrs)
+      conn = admin_login(conn)
+      conn = put(conn, Routes.permission_path(conn, :update, permission), permission: @invalid_attrs)
 
       assert html_response(conn, 200) =~ "Edit Permission"
     end
@@ -87,9 +84,8 @@ defmodule AuthWeb.PermissionControllerTest do
     setup [:create_permission]
 
     test "deletes chosen permission", %{conn: conn, permission: permission} do
-      conn =
-        admin_login(conn)
-        |> delete(Routes.permission_path(conn, :delete, permission))
+      conn = admin_login(conn)
+      conn = delete(conn, Routes.permission_path(conn, :delete, permission))
 
       assert redirected_to(conn) == Routes.permission_path(conn, :index)
 
