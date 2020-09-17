ecae729e361cf6e1ef202d34aa28ee7e10faa639
nelsonic <nelson+github@dwyl.com>
2020-09-17 10:12:42 +0100

    clarify reason for `list_apps/1 proxy function #116

--- lib/auth_web/controllers/role_controller.ex
+++ lib/auth_web/controllers/role_controller.ex
@@ -7,22 +7,15 @@ defmodule AuthWeb.RoleController do
 
   def index(conn, _params) do
     # restrict viewing to only roles owned by the person or default roles:
-    apps = list_apps(conn.assigns.person.id)
+    apps = list_apps(conn)
     app_ids = Enum.map(apps, fn a -> a.id end)
     roles = Role.list_roles_for_apps(app_ids)
     render(conn, "index.html", roles: roles)
   end
 
-  defp list_apps(person_id) do
-    case person_id == 1 do
-      true -> Auth.App.list_apps()
-      false -> Auth.App.list_apps(person_id)
-    end
-  end
-
   def new(conn, _params) do
     changeset = Role.change_role(%Role{})
-    apps = list_apps(conn.assigns.person.id)
+    apps = list_apps(conn)
     # Roles Ref/Require Apps: https://github.com/dwyl/auth/issues/112
     # Check if the person already has apps:
     if length(apps) > 0 do
@@ -36,7 +29,7 @@ defmodule AuthWeb.RoleController do
   end
 
   def create(conn, %{"role" => role_params}) do
-    apps = list_apps(conn.assigns.person.id)
+    apps = list_apps(conn)
     # check that the role_params.app_id is owned by the person:
     if person_owns_app?(apps, Map.get(role_params, "app_id")) do
       # never allow the request to define the person_id:
@@ -79,14 +72,14 @@ defmodule AuthWeb.RoleController do
       AuthWeb.AuthController.not_found(conn, "role not found.")
     else
       changeset = Role.change_role(role)
-      apps = list_apps(conn.assigns.person.id)
+      apps = list_apps(conn)
       render(conn, "edit.html", role: role, changeset: changeset, apps: apps)
     end
   end
 
   def update(conn, %{"id" => id, "role" => role_params}) do
     role = Role.get_role!(id, conn.assigns.person.id)
-    apps = list_apps(conn.assigns.person.id)
+    apps = list_apps(conn)
     # cannot update a role that doesn't exist (or they don't own):
     if is_nil(role) do
       AuthWeb.AuthController.not_found(conn, "role not found.")
@@ -100,7 +93,7 @@ defmodule AuthWeb.RoleController do
             |> redirect(to: Routes.role_path(conn, :show, role))
 
           {:error, %Ecto.Changeset{} = changeset} ->
-            apps = list_apps(conn.assigns.person.id)
+            apps = list_apps(conn)
             render(conn, "edit.html", role: role, changeset: changeset, apps: apps)
         end
       else
@@ -108,6 +101,17 @@ defmodule AuthWeb.RoleController do
       end
     end
   end
+
+  # Returning all apps when person_id == 1 (superadmin) means
+  # the superadmin can always see/manage all apps as necessary.
+  # Later we could refactor this function to use RBAC.has_role_any/2.
+  defp list_apps(conn) do
+    case conn.assigns.person.id == 1 do
+      true -> Auth.App.list_apps()
+      false -> Auth.App.list_apps(conn.assigns.person.id)
+    end
+  end
+
 
   # https://elixirforum.com/t/map-key-is-a-atom-or-string/13285/2
   # our use-case for this is specific keys in controller params
@@ -123,6 +127,7 @@ defmodule AuthWeb.RoleController do
   end
 
   def delete(conn, %{"id" => id}) do
+    # can only delete a role you own:
     role = Role.get_role!(id, conn.assigns.person.id)
 
     if is_nil(role) do
@@ -146,6 +151,7 @@ defmodule AuthWeb.RoleController do
     # confirm that the granter is either superadmin (conn.assigns.person.id == 1)
     # or has an "admin" role (1 || 2)
     granter_id = conn.assigns.person.id
+    # we need to expand grant priviledges see: https://github.com/dwyl/auth/issues/119
 
     if granter_id == 1 do
       role_id = map_get(params, "role_id")
