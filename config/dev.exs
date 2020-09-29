e1e850b949046ae602066ca346af84f51102f298
nelsonic <nelson+github@dwyl.com>
2020-09-29 20:31:51 +0100

    404.html.eex template for #128

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :auth, Auth.Repo,
 # with webpack to recompile .js and .css sources.
 config :auth, AuthWeb.Endpoint,
   http: [port: 4000],
-  debug_errors: true,
+  debug_errors: false,
   code_reloader: true,
   check_origin: false,
   watchers: [
--- lib/auth_web/views/error_view.ex
+++ lib/auth_web/views/error_view.ex
@@ -3,14 +3,28 @@ defmodule AuthWeb.ErrorView do
 
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
-  # def render("500.html", _assigns) do
-  #   "Internal Server Error"
+  # def render("404.html", _assigns) do
+  #   "Hello"
+  # end
+  def error_message(conn) do
+    if Map.has_key?(conn.assigns, :reason)
+      and Map.has_key?(conn.assigns.reason, :message) do
+      "Sorry, " <> conn.assigns.reason.message
+    else
+      "Sorry, that page could not be found."
+    end
+  end
+
+  # def debug(map) do
+  #   IO.inspect(map)
+  #   ""
   # end
 
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.html" becomes
   # "Not Found".
   def template_not_found(template, _assigns) do
+    IO.inspect(template, label: "template")
     Phoenix.Controller.status_message_from_template(template)
   end
 end
