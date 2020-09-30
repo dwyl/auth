d51dd2326da51db1f0fec3b9181099e6819b19d9
nelsonic <nelson+github@dwyl.com>
2020-09-30 08:58:51 +0100

    debug_errors: true (I had it as "false" while testing out Friendly Errors #128)

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :auth, Auth.Repo,
 # with webpack to recompile .js and .css sources.
 config :auth, AuthWeb.Endpoint,
   http: [port: 4000],
-  debug_errors: false,
+  debug_errors: true,
   code_reloader: true,
   check_origin: false,
   watchers: [
