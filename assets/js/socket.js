14a25ee64c1ff4e50a720468e33d8d5d1b1f6747
Nelson <194400+nelsonic@users.noreply.github.com>
2020-10-07 10:02:04 +0100

    add semicolon to .receive in socket.js to keep @sourcelevebot happy #104

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -58,6 +58,6 @@ socket.connect()
 let channel = socket.channel('topic:subtopic', {});
 channel.join()
   .receive('ok', resp => { console.log('Joined successfully', resp) });
-  .receive('error', resp => { console.log('Unable to join', resp) })
+  .receive('error', resp => { console.log('Unable to join', resp) });
 
 export default socket;
