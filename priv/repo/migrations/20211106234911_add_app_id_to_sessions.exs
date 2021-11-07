56c05bb697614a05a62581babb4f718649108ecc
nelsonic <nelson+github@dwyl.com>
2021-11-07 00:27:08 +0000

    create test/auth/session_test.exs with test for inserting session record

--- priv/repo/migrations/20211106234911_add_app_id_to_sessions.exs
+++ priv/repo/migrations/20211106234911_add_app_id_to_sessions.exs
@@ -5,7 +5,14 @@ defmodule Auth.Repo.Migrations.AddAppIdToSessions do
     alter table(:sessions) do
       add :app_id, references(:apps, on_delete: :nothing)
       add :auth_provider, :string
+      add :end_at, :utc_datetime 
+      remove :person_id # avoid tight coupling
       add :person_id, references(:people, on_delete: :nothing)
+      add :user_agent_id, :integer
+      # Don't want to risk leaking auth/refresh tokens in a breach
+      # so just not going to store them even encrypted. 
+      remove :auth_token
+      remove :refresh_token
     end
   end
 end
