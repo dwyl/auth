d9fb11f57e187a1d183798aafc01e2360f9d1142
nelsonic <nelson+github@dwyl.com>
2022-01-04 22:09:06 +0000

    debug File.cwd!() in release.ex #172

--- Dockerfile
+++ Dockerfile
@@ -63,7 +63,6 @@ COPY config/runtime.exs config/
 
 COPY rel rel
 RUN mix release
-RUN mix seeds
 
 # start a new build stage so that the final image will only contain
 # the compiled release and other runtime necessities
--- lib/auth/release.ex
+++ lib/auth/release.ex
@@ -11,6 +11,8 @@ defmodule Auth.Release do
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
+
+    IO.inspect(File.cwd!(), label: "cwd")
   end
 
   def rollback(repo, version) do
