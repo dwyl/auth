19bdd7e2ef49af304fbeb5f7b3a170b639738432
nelsonic <nelson+github@dwyl.com>
2022-01-05 00:19:42 +0000

    File.ls!("/app/bin/auth")

--- lib/auth/release.ex
+++ lib/auth/release.ex
@@ -29,6 +29,7 @@ defmodule Auth.Release do
     IO.inspect(File.ls!("/app/lib"), label: "File.ls!(/app/lib)")
     IO.puts(" - - - - - - - - - - - - - - - - - - - - - - - ")
     IO.inspect(File.ls!("/app/bin"), label: "File.ls!(/app/bin)")
+    IO.inspect(File.ls!("/app/bin/auth"), label: "File.ls!(/app/bin/auth)")
     IO.puts(" - - - - - - - - - - - - - - - - - - - - - - - ")
     IO.inspect(File.ls!("/app/releases"), label: "File.ls!(/app/releases)")
     # IO.puts("Code.eval_file: #{filepath}")
