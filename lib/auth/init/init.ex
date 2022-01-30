4c32cc5310d5cd369f50e2d941863f78301f5c92
nelsonic <nelson+github@dwyl.com>
2022-01-30 23:01:26 +0000

    tidy up init.ex comments #174

--- lib/auth/init/init.ex
+++ lib/auth/init/init.ex
@@ -1,6 +1,6 @@
 defmodule Auth.Init do
   @moduledoc """
-  Init as it's name suggests initializes the Auth Application 
+  `Init` as its' name suggests initializes the Auth Application 
   by creating the necessary records in the various tables.
 
   This is the sequence of steps that are followed to init the App:
@@ -8,12 +8,13 @@ defmodule Auth.Init do
   1. Create the "Super Admin" person who owns the Auth App
   based on the `ADMIN_EMAIL` environment/config variable.
   
-  > The person.id for the Super Admin will own the remaining records
-  so it needs to be created first. 
+  > The person.id (1) for the Super Admin 
+  will own the remaining records so it needs to be created first. 
 
   2. Create default records (Statuses & Roles)
 
-  3. Create the App and AUTH_API_KEY for the Auth App.
+  3. Create the App and `AUTH_API_KEY` for the Auth App.
+  > Log the `AUTH_API_KEY` so that it can be exported.
   """
 
   require Logger
@@ -21,9 +22,9 @@ defmodule Auth.Init do
   alias Auth.{Person, Role, Repo, Status}
 
   def main do
-    Logger.debug("Initialising the Auth Database ...")
+    Logger.info("Initialising the Auth Database ...")
     # check required environment variables:
-    Envar.is_set_all?(~w/ADMIN_EMAIL ENCRYPTION_KEYS SECRET_KEY_BASE/)
+    Envar.is_set_all?(~w/ADMIN_EMAIL AUTH_URL ENCRYPTION_KEYS SECRET_KEY_BASE/)
 
     admin = Auth.Init.create_admin()
 
@@ -31,20 +32,20 @@ defmodule Auth.Init do
     Auth.Init.create_default_roles()
     
     api_key = Auth.Init.create_apikey_for_admin(admin)
-
-    # set the AUTH_API_KEY environment variable during test run:
-    # IO.inspect(Mix.env(), label: "Mix.env()")
-    # coveralls-ignore-start
+ 
     case Mix.env() do
       :test ->
+        # set the AUTH_API_KEY environment variable during test run:
         Envar.set("AUTH_API_KEY", api_key)
-
+      # ignore the next lines because we can't test them:
+      # coveralls-ignore-start
       _ ->
+        # Log the AUTH_API_KEY so it can be exported:
         Logger.info("export AUTH_API_KEY=#{api_key}")
+      # coveralls-ignore-stop
     end
-    # coveralls-ignore-stop
-    
-    # Update status of Admin to "Verified"
+
+    # Update status of Admin to "verified"
     Auth.Person.verify_person_by_id(1)
     
     # grant superadmin role to app owner:
@@ -53,9 +54,7 @@ defmodule Auth.Init do
     :ok
   end
 
-
-  # This is used exclusivvely during setup
-  # don't confuse it with AuthPlug.get_auth_url/2
+  # Get AUTH_URL or fallback to localhost:
   defp get_auth_url do
     # see .env_sample for example
     Envar.get("AUTH_URL") || "localhost:4000"
@@ -69,7 +68,6 @@ defmodule Auth.Init do
       nil ->
         %Person{}
         |> Person.changeset(%{email: email})
-        # |> put_assoc(:statuses, [%Status{text: "verified"}])
         |> Repo.insert!()
       # coveralls-ignore-stop
 
@@ -82,14 +80,14 @@ defmodule Auth.Init do
     {:ok, app} =
       %{
         "name" => "default system app",
-        "desc" => "Created by /priv/repo/seeds.exs during setup.",
+        "desc" => "Created by lib/auth/init/init.ex during setup.",
         "url" => "localhost:4000",
         "person_id" => person.id,
         "status" => 3
       }
       |> Auth.App.create_app()
 
-    # set the api key to AUTH_API_KEY in env:
+    # If AUTH_API_KEY environment variable is already set, use it:
     update_attrs = %{
       "client_id" => AuthPlug.Token.client_id(),
       "client_secret" => AuthPlug.Token.client_secret()
@@ -106,7 +104,6 @@ defmodule Auth.Init do
   # scripts for creating default roles and permissions
   def get_json(filepath) do
     path = File.cwd!() <> filepath
-    # IO.inspect(path, label: "path")
     {:ok, data} = File.read(path)
     json = Jason.decode!(data)
     json
@@ -123,5 +120,4 @@ defmodule Auth.Init do
       Status.upsert_status(status)
     end)
   end
-
 end
