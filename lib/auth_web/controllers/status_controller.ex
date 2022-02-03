5f2a86fdba818dfe6707e443098646bdf45fcfbd
nelsonic <nelson+github@dwyl.com>
2022-02-03 22:11:19 +0000

    init cannot be run until all required environment variables are set #176

--- lib/auth_web/controllers/status_controller.ex
+++ lib/auth_web/controllers/status_controller.ex
@@ -5,12 +5,20 @@ defmodule AuthWeb.StatusController do
   @env_optional ~w/EMAIL_APP_URL GITHUB_CLIENT_ID GITHUB_CLIENT_SECRET GOOGLE_CLIENT_ID GOOGLE_CLIENT_SECRET/
 
   def index(conn, _params) do
+
+    init = if Envar.is_set_all?(@env_required) do
+      Auth.Init.main()
+    else
+      "cannot be run until all the required environment variables are set"
+    end
+
     conn
     # |> assign(:env, check_env())
     |> render(:index,
       layout: {AuthWeb.StatusView, "status_layout.html"}, 
       env: check_env(@env_required),
-      env_optional: check_env(@env_optional)
+      env_optional: check_env(@env_optional),
+      init: init
       )
   end
 
