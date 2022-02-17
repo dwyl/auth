37eb905a4f57bc115a8ade6def1142b7650e13e8
nelsonic <nelson+github@dwyl.com>
2022-02-17 23:43:17 +0000

    src="https://raw.githubusercontent.com/dwyl/auth/main/assets/static/images/true.png" fixes #178

--- lib/auth_web/views/init_view.ex
+++ lib/auth_web/views/init_view.ex
@@ -1,15 +1,3 @@
 defmodule AuthWeb.InitView do
   use AuthWeb, :view
-
-  def status_image(true_false) do
-    base = "https://user-images.githubusercontent.com/194400/"
-    t = "154710782-66ab7319-8307-4cd4-9e5a-497990b86c7a.png"
-    f = "154711150-3fde9f49-eed5-4e1b-bd2a-f9f52afee65a.png"
-
-    IO.inspect(true_false, label: "status_image/9 true_false")
-    case true_false do
-      true -> base <> t
-      false -> base <> f
-    end
-  end
 end
