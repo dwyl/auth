86c7b2e7ec1410450c52c1dd5366ddf1eaa667b2
SimonLab <simon.labondance@gmail.com>
2022-03-14 09:58:00 +0000

    update auth version to 1.6.6 from 1.6.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Auth.Mixfile do
   def project do
     [
       app: :auth,
-      version: "1.6.5",
+      version: "1.6.6",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
